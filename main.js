2001988f88485463dcddb83fd36972d10cb30012
Mahendra <mahendra.k12@gmail.com>
2019-01-28 14:18:24 +0530

    conditional rendering

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ var app = new Vue({
 	el: "#app",
 	data: {
 		product: "socks",
-		description: "A pair of warm, fuzzy socks"
+		description: "A pair of warm, fuzzy socks",
+		image: "https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
+		link: "https://google.com",
+		inventory: 8,
+		onSale: true
 	}
 })
 

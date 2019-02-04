e660b503601248fd631c6d793935d2403b014acf
Mahendra <mahendra.k12@gmail.com>
2019-02-04 14:28:27 +0530

    class and style bindings

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = new Vue({
 	data: {
 		product: "socks",
 		description: "A pair of warm, fuzzy socks",
-		image: "./green.jpg",
+		image: "./assets/blue.jpg",
 		link: "https://google.com",
 		inventory: 8,
 		onSale: true,
@@ -12,15 +12,29 @@ var app = new Vue({
 			{
 				variantId: 2234,
 				variantColor: "green",
+				variantImage: "./assets/green.jpg",
 			},
 			{
 				variantId: 2235,
 				variantColor: "blue",
+				variantImage: "./assets/blue.jpg",
 
 			}
 		],
 		sizes: ["small", "regular", "large"],
-		cart: 0
+		cart: 0,
+		inStock: true
+	},
+	methods: {
+		addToCart(){
+			this.cart += 1
+		},
+		removeFromCart(){
+			this.cart -= 1
+		},
+		updateProduct(variantImage) {
+			this.image = variantImage
+		}
 	}
 })
 
